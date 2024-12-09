26d166c2a946b0b4e57bee66ac84c88eca1d5a68
earvin(vivopc) <lin.earvin@gmail.com(vivopc)>
2024-12-10 07:35:02 +0800

    20241210-0734 add code.

---
+++ build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_QTEarvinStocksPGMs_FILE E:/WORKSPACES/GithubProjects/QTProjects/QTEarvinSTKPGMs/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/QTEarvinStocksPGMs.exe)
+set(__QT_DEPLOY_TARGET_QTEarvinStocksPGMs_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_QTEarvinStocksPGMs_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
